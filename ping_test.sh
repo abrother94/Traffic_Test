bec74da3becafd54ed4c3b5c5853782bfe507b7d
abrother94 <abrother94@gmail.com>
2020-03-24 11:50:30 +0800

    update

---
+++ ping_test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+IP=$1
+
+if [ "$IP" = "" ];then
+    echo "Need IP address!!"
+    exit
+fi
+
+COUNT=5
+`sudo killall dhclient 2> /dev/null`
+`sudo killall dhclient 2> /dev/null`
+`sudo ifconfig eth0 0.0.0.0`
+`sudo dhclient eth0 2> /dev/null`
+RES=`ping -I eth0 $IP -i 0.2 -c $COUNT -W 1 | grep '64 bytes from' | wc -l`
+if [ $RES == $COUNT ];then
+    echo PASS "($RES)/($COUNT)"
+else
+    echo FAIL "($RES)/($COUNT)"
+fi
+
